fbffd930f1adde1252f81857aefb5223e18a12d3
Yeongjae Kim <sees77770@gmail.com>
2024-06-11 20:44:49 +0900

    userCreateController 테스트 코드 추가

--- src/test/java/com/example/demo/controller/UserCreateControllerTest.java
+++ src/test/java/com/example/demo/controller/UserCreateControllerTest.java
@@ -4,29 +4,60 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlGroup;
 import org.springframework.test.web.servlet.MockMvc;
 
+import com.example.demo.model.dto.UserCreateDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import static org.mockito.ArgumentMatchers.any;
+
 @SpringBootTest
-@TestPropertySource("classpath:test-application.properties")
 @AutoConfigureMockMvc
 @AutoConfigureTestDatabase
+@TestPropertySource("classpath:test-application.properties")
+@SqlGroup({
+	@Sql(value = "/sql/delete-all-data.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+})
 public class UserCreateControllerTest {
 
 	@Autowired
 	private MockMvc mockMvc;
+	@MockBean
+	private JavaMailSender mailSender;
+	private final ObjectMapper objectMapper = new ObjectMapper();
 
 	@Test
-	void 헬스_체크_응답이_200으로_내려온다() throws Exception {
+	void 사용자는_회원_가입을_할_수있고_회원가입된_사용자는_PENDING_상태이다() throws Exception {
 		// given
+		UserCreateDto userCreateDto = UserCreateDto.builder()
+			.email("kyj91032@naver.com")
+			.nickname("kyj")
+			.address("Pangyo")
+			.build();
+		BDDMockito.doNothing().when(mailSender).send(any(SimpleMailMessage.class));
+
 		// when
 		// then
-		mockMvc.perform(get("/health_check.html"))
-			.andExpect(status().isOk());
+		mockMvc.perform(
+				post("/api/users")
+					.contentType(MediaType.APPLICATION_JSON)
+					.content(objectMapper.writeValueAsString(userCreateDto)))
+			.andExpect(status().isCreated())
+			.andExpect(jsonPath("$.id").isNumber())
+			.andExpect(jsonPath("$.email").value("kyj91032@naver.com"))
+			.andExpect(jsonPath("$.nickname").value("kyj"))
+			.andExpect(jsonPath("$.status").value("PENDING"));
 	}
 
 }
