894e55a5dc8bfd052b4d86fe7b24d8b31494c8a6
Yeongjae Kim <sees77770@gmail.com>
2024-06-23 13:40:11 +0900

    refactor: MailSender에 의존성 역전 적용

--- src/main/java/com/example/demo/user/service/UserService.java
+++ src/main/java/com/example/demo/user/service/UserService.java
@@ -24,7 +24,7 @@ import lombok.RequiredArgsConstructor;
 public class UserService {
 
     private final UserRepository userRepository;
-    private final JavaMailSender mailSender;
+    private final CertificationService certificationService;
 
     @Value("${spring.mail.username}")
     private String fromEmail;
@@ -48,8 +48,7 @@ public class UserService {
         userEntity.setStatus(UserStatus.PENDING);
         userEntity.setCertificationCode(UUID.randomUUID().toString());
         userEntity = userRepository.save(userEntity);
-        String certificationUrl = generateCertificationUrl(userEntity);
-        sendCertificationEmail(userCreate.getEmail(), certificationUrl);
+        certificationService.send(userCreate.getEmail(), userEntity.getId(), userEntity.getCertificationCode());
         return userEntity;
     }
 
@@ -77,27 +76,4 @@ public class UserService {
         userEntity.setStatus(UserStatus.ACTIVE);
     }
 
-    private void sendCertificationEmail(String email, String certificationUrl) {
-        // MimeMessage message = mailSender.createMimeMessage();
-        // MimeMessageHelper helper = new MimeMessageHelper(message);
-        // try {
-        //     helper.setTo(email);
-        //     helper.setSubject("Please certify your email address");
-        //     helper.setText("Please click the following link to certify your email address: " + certificationUrl);
-        //     helper.setFrom(fromEmail);
-        // } catch (Exception e) {
-        //     throw new RuntimeException(e);
-        // }
-        // mailSender.send(message);
-
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(email);
-        message.setSubject("Please certify your email address");
-        message.setText("Please click the following link to certify your email address: " + certificationUrl);
-        mailSender.send(message);
-    }
-
-    private String generateCertificationUrl(UserEntity userEntity) {
-        return "http://localhost:8080/api/users/" + userEntity.getId() + "/verify?certificationCode=" + userEntity.getCertificationCode();
-    }
 }
